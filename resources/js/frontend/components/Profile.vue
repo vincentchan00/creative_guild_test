1d8e0ad8d68da57ce1db48eca79a8b135aa01799
Singho Chan <vincent@chicle.com.hk>
2022-06-17 17:01:50 -0400

    profile size update

--- resources/js/frontend/components/Profile.vue
+++ resources/js/frontend/components/Profile.vue
@@ -1,30 +1,34 @@
 <template>
     <div class="my-4">
-        <div class="row profile main p-4 ">
-            <div class="icon">
+        <div class="row profile main p-3 no-gutter">
+            <div class="icon col-3">
                 <img v-bind:src="'/'+photographer.profile_picture" class="w-100 rounded-circle">
             </div>
-            <div class="col w-50">
+            <div class="col-9">
                 <h1 class="font-weight-bold">{{photographer.name}}</h1>
-                <div>
-                    <div class="font-weight-bold text-secondary">Bio</div>
-                    <div class="text-muted">{{photographer.bio}}</div>
-                </div>
-            </div>
-            <div class="col w-25">
-                <div>
-                    <div class="font-weight-bold text-secondary">Phone</div>
-                    <div style="color:#e661a6;">
-                        {{photographer.phone}}
+                <div class="row no-gutter">
+                    <div class="col-8" style:font-size:14px>
+                        <div class="font-weight-bold text-secondary">Bio</div>
+                        <div class="text-muted">{{photographer.bio}}</div>
+                    </div>
+                    <div class="col-4">
+                        <div>
+                            <div class="font-weight-bold text-secondary">Phone</div>
+                            <div style="color:#e661a6;">
+                                {{photographer.phone}}
+                            </div>
+                        </div>
+                        <div>
+                            <div class="font-weight-bold text-secondary">Email</div>
+                            <div style="color:#e661a6;">
+                                {{photographer.email}}
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <div class="font-weight-bold text-secondary">Email</div>
-                    <div style="color:#e661a6;">
-                        {{photographer.email}}
-                    </div>
-                </div>
+
             </div>
+
         </div>
         <div class="row main m-2 no-gutters">
             <gallery-card v-for="info in photographer['album'] "
@@ -69,6 +73,7 @@
     }
     .profile .icon {
          width: 20%;
+        padding: 30px;
     }
 
 </style>
